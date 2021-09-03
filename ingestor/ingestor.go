7fd3dedee582faf958d58a747ac39bfa79c2698c
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-09-02 23:43:09 -0500

    add parsing for ingestor config to main and define simple ingestor

---
+++ ingestor/ingestor.go
@@ -0,0 +1,21 @@
+package ingestor
+
+import "log"
+
+type Ingestor struct {
+	logger *log.Logger
+	discordAPIKey string
+	redisEndpoints []string
+}
+
+func New(logger *log.Logger, discordAPIKey string, redisEndpoints[] string) Ingestor {
+	return Ingestor{
+		logger: logger,
+		discordAPIKey: discordAPIKey,
+		redisEndpoints: redisEndpoints,
+	}
+}
+
+func (ingestor Ingestor) Open() error {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"github.com/SomethingBot/discordingestor/ingestor"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
+var (
+	GitCommit string
+	GitTag    string
+	Mode      string
+)
+
+func main() {
+	logger := log.New(os.Stdout, "", log.LUTC|log.Ldate|log.Ltime)
+	logger.Printf("Starting discordingestor, commit:%v, tag:%v, Mode:%v", GitCommit, GitTag, Mode)
+
+	var discordAPIKey string
+	if discordAPIKeyFile := os.Getenv("DISCORDAPIKEYFILE"); discordAPIKeyFile != "" {
+		file, err := os.Open(discordAPIKeyFile)
+		if err != nil {
+			logger.Printf("Could not open file (%v), error (%v)", discordAPIKeyFile, err)
+			os.Exit(1)
+		}
+
+		fileStat, err := file.Stat()
+		if err != nil {
+			logger.Printf("Could not get file (%v) stats (%v)", file.Name(), err)
+		}
+
+		if fileStat.Size() > 1<<20 {
+			logger.Printf("Notice: file (%v) size is larger than 1MB", file.Name())
+		}
+
+		body, err := io.ReadAll(file)
+		if err != nil {
+			logger.Printf("Opened, but could not read file (%v), error (%v)", discordAPIKeyFile, err)
+			os.Exit(1)
+		}
+
+		discordAPIKey = string(body)
+	}
+
+	var redisEndpoints []string
+	redisEndpointsEnv := os.Getenv("REDISENDPOINTS")
+	if !strings.Contains(redisEndpointsEnv, ",") {
+		redisEndpoints = []string{redisEndpointsEnv}
+	}
+	redisEndpoints = strings.Split(redisEndpointsEnv, ",")
+
+	ingest := ingestor.New(logger, discordAPIKey, redisEndpoints)
+	err := ingest.Open()
+	if err != nil {
+		logger.Printf("Could not open ingestor (%v)", err)
+		return
+	}
+
+	logger.Printf("Stopping discordingestor, commit:%v, tag:%v, Mode:%v", GitCommit, GitTag, Mode)
 }
