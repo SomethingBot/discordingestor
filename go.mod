4fe5232ab7a5a16f281ec1abc2ce6169946542e4
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-09-04 16:32:24 -0500

    add api package and start defining guild

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/SomethingBot/discordingestor
 
 go 1.16
+
+require (
+	github.com/SomethingBot/discordgo v0.23.3-0.20210819034142-c439deaedd07 // indirect
+	github.com/go-redis/redis/v8 v8.11.3 // indirect
+)
--- ingestor/ingestor.go
+++ ingestor/ingestor.go
@@ -1,21 +1,79 @@
 package ingestor
 
-import "log"
+import (
+	"fmt"
+	"github.com/SomethingBot/discordgo"
+	"log"
+	"strings"
+	"sync"
+)
+
+var (
+	ErrorAlreadyOpen   = fmt.Errorf("ingestor already open")
+	ErrorAlreadyClosed = fmt.Errorf("ingestor already closed")
+)
+
+type RedisConfig struct {
+	redisEndPoints []string
+}
+
+type DiscordConfig struct {
+	discordAPIKey    string
+	discordGoSession *discordgo.Session
+}
 
 type Ingestor struct {
-	logger         *log.Logger
-	discordAPIKey  string
-	redisEndpoints []string
+	logger   *log.Logger
+	open     bool
+	openLock sync.Mutex
+	RedisConfig
+	DiscordConfig
 }
 
-func New(logger *log.Logger, discordAPIKey string, redisEndpoints []string) Ingestor {
-	return Ingestor{
-		logger:         logger,
-		discordAPIKey:  discordAPIKey,
-		redisEndpoints: redisEndpoints,
+func New(logger *log.Logger, discordAPIKey string, redisEndpoints []string) (ingestor Ingestor) {
+	ingestor.logger = logger
+	ingestor.discordAPIKey = discordAPIKey
+	ingestor.redisEndPoints = redisEndpoints
+	return
+}
+
+func (ingestor *Ingestor) Open() (err error) {
+	ingestor.openLock.Lock()
+	defer ingestor.openLock.Unlock()
+	if ingestor.open {
+		return ErrorAlreadyOpen
 	}
+	ingestor.open = true
+
+	ingestor.discordGoSession, err = discordgo.New("Bot " + strings.TrimSuffix(ingestor.discordAPIKey, "\n"))
+	if err != nil {
+		return err
+	}
+
+	ingestor.discordGoSession.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages | discordgo.IntentsGuildInvites | discordgo.IntentsGuildVoiceStates | discordgo.IntentsGuilds)
+
+	ingestor.discordGoSession.AddHandler(ingestor.handleMessages)
+
+	err = ingestor.discordGoSession.Open()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (ingestor Ingestor) Open() error {
+func (ingestor *Ingestor) Close() (err error) {
+	ingestor.openLock.Lock()
+	defer ingestor.openLock.Unlock()
+	if !ingestor.open {
+		return ErrorAlreadyClosed
+	}
+	ingestor.open = false
+
+	err = ingestor.discordGoSession.Close()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
