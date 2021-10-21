e0f68aa979e6846cf18dfc8f2efa15a6c16181fd
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-20 23:35:15 -0500

    move arikawa client to another package

---
+++ arikawa/arikawa.go
@@ -0,0 +1,28 @@
+package arikawa
+
+import "github.com/SomethingBot/discordingestor/ingestor"
+
+type Session struct {
+	//apikey without the "Bot " prefix
+	apikey string
+}
+
+func New(apikey string) ingestor.DiscordClient {
+	return &Session{apikey: apikey}
+}
+
+func (arikawaSession *Session) Open() error {
+	return nil
+}
+
+func (arikawaSession *Session) Close() error {
+	return nil
+}
+
+func (arikawaSession *Session) AddHandler(handlerFunc func(string)) error {
+	return nil
+}
+
+func (arikawaSession *Session) SetIntents(discordIntent ingestor.DiscordIntent) {
+
+}
