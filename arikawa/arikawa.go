0c0f7546a63a3dc698ec48ec49346b74cde9e702
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-22 15:25:41 -0500

    document Arikawa Client

--- arikawa/arikawa.go
+++ arikawa/arikawa.go
@@ -5,27 +5,33 @@ import (
 	"github.com/SomethingBot/discordingestor/ingestor"
 )
 
+//Session implements the DiscordClient and works off an embed Arikawa client
 type Session struct {
 	//apikey without the "Bot " prefix
 	apikey string
 }
 
+//New DiscordClient from a Session
 func New(apikey string) ingestor.DiscordClient {
 	return &Session{apikey: apikey}
 }
 
+//Open Arikawa Session
 func (arikawaSession *Session) Open() error {
 	return nil
 }
 
+//Close Arikawa Session
 func (arikawaSession *Session) Close() error {
 	return nil
 }
 
+//AddHandler function to be called on an Event
 func (arikawaSession *Session) AddHandler(handlerFunc func(string)) error {
 	return nil
 }
 
+//SetIntents to DiscordGateway
 func (arikawaSession *Session) SetIntents(discordIntent discordprimatives.GatewayIntent) {
 
 }
