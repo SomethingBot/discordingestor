fcd0bceee24491cfc91632c5a7e27a58fb791446
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-28 15:22:57 -0500

    add gateway intents

--- discordprimatives/gateway.go
+++ discordprimatives/gateway.go
@@ -1,4 +1,105 @@
 package discordprimatives
 
 //GatewayIntent from https://discord.com/developers/docs/topics/gateway#gateway-intents
-type GatewayIntent uint32
+type GatewayIntent uint16
+
+const (
+	//GatewayIntentNil is when no GatewayIntent is set
+	GatewayIntentNil GatewayIntent = 0
+	//GatewayIntentGuilds contains events:
+	//- GuildCreate
+	//- GuildUpdate
+	//- GuildDelete
+	//- GuildRoleCreate
+	//- GuildRoleUpdate
+	//- GuildRoleDelete
+	//- ChannelCreate
+	//- ChannelUpdate
+	//- ChannelDelete
+	//- ChannelPinsUpdate
+	//- ThreadCreate
+	//- ThreadUpdate
+	//- ThreadDelete
+	//- ThreadListSync
+	//- ThreadMemberUpdate
+	//- ThreadMembersUpdate; data is different depending on intents used
+	//- StageInstanceCreate
+	//- StageInstanceUpdate
+	//- StageInstanceDelete
+	GatewayIntentGuilds GatewayIntent = 1 << (iota - 1)
+	//GatewayIntentGuildMembers contains events:
+	//- GuildMemberAdd
+	//- GuildMemberUpdate
+	//- GuildMemberRemove
+	//- ThreadMembersUpdate *
+	GatewayIntentGuildMembers
+	//GatewayIntentGuildBans contains events:
+	//- GuildBanAdd
+	//- GuildBanRemove
+	GatewayIntentGuildBans
+	//GatewayIntentGuildEmojisAndStickers contains events:
+	//- GuildEmojisUpdate
+	//- GuildStickersUpdate
+	GatewayIntentGuildEmojisAndStickers
+	//GatewayIntentGuildIntegrations contains events:
+	//- GuildIntegrationsUpdate
+	//- IntegrationCreate
+	//- IntegrationUpdate
+	//- IntegrationDelete
+	GatewayIntentGuildIntegrations
+	//GatewayIntentGuildWebhooks contains events:
+	//- WebhooksUpdate
+	GatewayIntentGuildWebhooks
+	//GatewayIntentGuildInvites contains events:
+	//- InviteCreate
+	//- InviteDelete
+	GatewayIntentGuildInvites
+	//GatewayIntentGuildVoiceStates contains events:
+	//- VoiceStateUpdate
+	GatewayIntentGuildVoiceStates
+	//GatewayIntentGuildPresences contains events:
+	//- PresenceUpdate
+	GatewayIntentGuildPresences
+	//GatewayIntentGuildMessages contains events:
+	//- MessageCreate
+	//- MessageUpdate
+	//- MessageDelete
+	//- MessageDeleteBulk
+	GatewayIntentGuildMessages
+	//GatewayIntentGuildMessageReactions contains events:
+	//- MessageReactionAdd
+	//- MessageReactionRemove
+	//- MessageReactionRemoveAll
+	//- MessageReactionRemoveEmoji
+	GatewayIntentGuildMessageReactions
+	//GatewayIntentGuildMessageTyping contains events:
+	//- TypingStart
+	GatewayIntentGuildMessageTyping
+	//GatewayIntentDirectMessages contains events:
+	//- MessageCreate
+	//- MessageUpdate
+	//- MessageDelete
+	//- ChannelPinsUpdate
+	GatewayIntentDirectMessages
+	//GatewayIntentDirectMessageReactions contains events:
+	//- MessageReactionAdd
+	//- MessageReactionRemove
+	//- MessageReactionRemoveAll
+	//- MessageReactionRemoveEmoji
+	GatewayIntentDirectMessageReactions
+	//GatewayIntentDirectMessageTyping contains events:
+	//- TypingStart
+	GatewayIntentDirectMessageTyping
+	//GatewayIntentAll is a combination of all known GatewayIntents
+	GatewayIntentAll GatewayIntent = (1 << (iota - 1)) - 1
+)
+
+//IsValid GatewayIntent
+func (gatewayIntent GatewayIntent) IsValid() bool {
+	return GatewayIntentAll&gatewayIntent == gatewayIntent && gatewayIntent != GatewayIntentNil
+}
+
+//Contains another GatewayIntent
+func (gatewayIntent GatewayIntent) Contains(intent GatewayIntent) bool {
+	return intent&gatewayIntent == intent && intent != GatewayIntentNil
+}
---
+++ discordprimatives/gateway_test.go
@@ -0,0 +1,227 @@
+package discordprimatives
+
+import "testing"
+
+func TestGatewayIntent_IsValid(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name          string
+		gatewayIntent GatewayIntent
+		want          bool
+	}{
+		{
+			name:          "GatewayIntentNil",
+			gatewayIntent: GatewayIntentNil,
+			want:          false,
+		},
+		{
+			name:          "GatewayIntentGuilds",
+			gatewayIntent: GatewayIntentGuilds,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMembers",
+			gatewayIntent: GatewayIntentGuildMembers,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildBans",
+			gatewayIntent: GatewayIntentGuildBans,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildEmojisAndStickers",
+			gatewayIntent: GatewayIntentGuildEmojisAndStickers,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildIntegrations",
+			gatewayIntent: GatewayIntentGuildIntegrations,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildWebhooks",
+			gatewayIntent: GatewayIntentGuildWebhooks,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildInvites",
+			gatewayIntent: GatewayIntentGuildInvites,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildVoiceStates",
+			gatewayIntent: GatewayIntentGuildVoiceStates,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildPresences",
+			gatewayIntent: GatewayIntentGuildPresences,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMessages",
+			gatewayIntent: GatewayIntentGuildMessages,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMessageReactions",
+			gatewayIntent: GatewayIntentGuildMessageReactions,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMessageTyping",
+			gatewayIntent: GatewayIntentGuildMessageTyping,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentDirectMessages",
+			gatewayIntent: GatewayIntentDirectMessages,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentDirectMessageReactions",
+			gatewayIntent: GatewayIntentDirectMessageReactions,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentDirectMessageTyping",
+			gatewayIntent: GatewayIntentDirectMessageTyping,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentAll",
+			gatewayIntent: GatewayIntentAll,
+			want:          true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := tt.gatewayIntent.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGatewayIntent_Contains(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name          string
+		gatewayIntent GatewayIntent
+		intent        GatewayIntent
+		want          bool
+	}{
+		{
+			name:          "GatewayIntentNil",
+			gatewayIntent: GatewayIntentNil,
+			intent:        GatewayIntentAll,
+			want:          false,
+		},
+		{
+			name:          "GatewayIntentGuilds",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuilds,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMembers",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildMembers,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildBans",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildBans,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildEmojisAndStickers",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildEmojisAndStickers,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildIntegrations",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildIntegrations,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildWebhooks",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildWebhooks,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildInvites",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildInvites,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildVoiceStates",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildVoiceStates,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildPresences",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildPresences,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMessages",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildMessages,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMessageReactions",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildMessageReactions,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentGuildMessageTyping",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentGuildMessageTyping,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentDirectMessages",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentDirectMessages,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentDirectMessageReactions",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentDirectMessageReactions,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentDirectMessageTyping",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentDirectMessageTyping,
+			want:          true,
+		},
+		{
+			name:          "GatewayIntentAll",
+			gatewayIntent: GatewayIntentAll,
+			intent:        GatewayIntentAll,
+			want:          true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := tt.gatewayIntent.Contains(tt.intent); got != tt.want {
+				t.Errorf("Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
