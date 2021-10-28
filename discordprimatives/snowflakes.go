b688e8129d67db7f5f39ea97da6beddacf89748c
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-28 01:21:53 -0500

    snowflakes are 64bit

--- discordprimatives/snowflakes.go
+++ discordprimatives/snowflakes.go
@@ -1,4 +1,4 @@
 package discordprimatives
 
 //Snowflake documented at https://discord.com/developers/docs/reference#snowflakes
-type Snowflake uint32 //todo: helper methods
+type Snowflake uint64 //todo: helper methods
