a5df1a5dbe354b542321b77a861f163d3df1f486
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-29 02:03:15 -0500

    add snowflake functions

--- discordprimatives/snowflakes.go
+++ discordprimatives/snowflakes.go
@@ -1,4 +1,38 @@
 package discordprimatives
 
+import (
+	"time"
+)
+
 //Snowflake documented at https://discord.com/developers/docs/reference#snowflakes
 type Snowflake uint64 //todo: helper methods
+
+const (
+	DiscordEpoch int64 = 1420070400000
+)
+
+//IsValid Snowflake
+func (snowflake Snowflake) IsValid() bool {
+	return snowflake != 0
+}
+
+//Timestamp of Snowflake as a time.Time UTC
+func (snowflake Snowflake) Timestamp() time.Time {
+	snowflakeTimeMilli := int64(snowflake>>22) + DiscordEpoch
+	return time.Unix(snowflakeTimeMilli/1e3, (snowflakeTimeMilli%1e3)*1e6).UTC()
+}
+
+//InternalWorkerID used by Discord
+func (snowflake Snowflake) InternalWorkerID() uint8 {
+	return uint8((snowflake & 0x3E0000) >> 17)
+}
+
+//InternalProcessID used by Discord
+func (snowflake Snowflake) InternalProcessID() uint8 {
+	return uint8((snowflake & 0x1F000) >> 12)
+}
+
+//Increment (ed) for every ID generated on InternalProcessID
+func (snowflake Snowflake) Increment() uint16 {
+	return uint16(snowflake & 0xFFF)
+}
---
+++ discordprimatives/snowflakes_test.go
@@ -0,0 +1,117 @@
+package discordprimatives
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestSnowflake_IsValid(t *testing.T) {
+	tests := []struct {
+		name      string
+		snowflake Snowflake
+		want      bool
+	}{
+		{
+			name:      "from docs",
+			snowflake: 175928847299117063,
+			want:      true,
+		},
+		{
+			name:      "nil snowflake",
+			snowflake: 0,
+			want:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.snowflake.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSnowflake_Timestamp(t *testing.T) {
+	tests := []struct {
+		name      string
+		snowflake Snowflake
+		want      time.Time
+	}{
+		{
+			name:      "from docs",
+			snowflake: 175928847299117063,
+			want:      time.Date(2016, time.April, 30, 11, 18, 25, 796*1e6, time.UTC),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.snowflake.Timestamp(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Timestamp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSnowflake_InternalWorkerID(t *testing.T) {
+	tests := []struct {
+		name      string
+		snowflake Snowflake
+		want      uint8
+	}{
+		{
+			name:      "from docs",
+			snowflake: 175928847299117063,
+			want:      1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.snowflake.InternalWorkerID(); got != tt.want {
+				t.Errorf("InternalWorkerID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSnowflake_InternalProcessID(t *testing.T) {
+	tests := []struct {
+		name      string
+		snowflake Snowflake
+		want      uint8
+	}{
+		{
+			name:      "from docs",
+			snowflake: 175928847299117063,
+			want:      0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.snowflake.InternalProcessID(); got != tt.want {
+				t.Errorf("InternalProcessID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSnowflake_Increment(t *testing.T) {
+	tests := []struct {
+		name      string
+		snowflake Snowflake
+		want      uint16
+	}{
+		{
+			name:      "from docs",
+			snowflake: 175928847299117063,
+			want:      7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.snowflake.Increment(); got != tt.want {
+				t.Errorf("Increment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
