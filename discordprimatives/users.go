c7606ccf03e7fb36c32b11ecc4bf10e1e77d827f
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-31 18:25:37 -0500

    add users

--- discordprimatives/users.go
+++ discordprimatives/users.go
@@ -24,6 +24,105 @@ type PresenceUpdate struct {
 	ClientStatus ClientStatus `json:"client_status"`
 }
 
+//UserFlag documented at https://discord.com/developers/docs/resources/user#user-object-user-flags
+type UserFlag uint32
+
+const (
+	//UserFlagNil is a User without any UserFlag(s)
+	UserFlagNil UserFlag = 0
+	//UserFlagDiscordEmployee is a Discord Employee
+	UserFlagDiscordEmployee UserFlag = 1 << 0
+	//UserFlagPartneredServerOwner is a Partnered Server Owner
+	UserFlagPartneredServerOwner UserFlag = 1 << 1
+	//UserFlagHypeSquadEvents is a HypeSquad Event coordinator
+	UserFlagHypeSquadEvents UserFlag = 1 << 2
+	//UserFlagBugHunterLevel1 is a Level 1 Bug Hunter
+	UserFlagBugHunterLevel1 UserFlag = 1 << 3
+	//UserFlagHouseBravery is a User that is part of House Bravery
+	UserFlagHouseBravery UserFlag = 1 << 6
+	//UserFlagHouseBrilliance is a User that is part of House Brilliance
+	UserFlagHouseBrilliance UserFlag = 1 << 7
+	//UserFlagHouseBalance is a User that is part of House Balance
+	UserFlagHouseBalance UserFlag = 1 << 8
+	//UserFlagEarlySupporter is an Early Nitro Supporter
+	UserFlagEarlySupporter UserFlag = 1 << 9
+	//UserFlagTeamUser is a bot team user? todo: don't actually know about this
+	UserFlagTeamUser UserFlag = 1 << 10
+	//UserFlagBugHunterLevel2 is Level 2 Bug Hunter
+	UserFlagBugHunterLevel2 UserFlag = 1 << 14
+	//UserFlagVerifiedBot is a Bot that has gone through the Verification process
+	UserFlagVerifiedBot UserFlag = 1 << 16
+	//UserFlagEarlyVerifiedBotDeveloper is a User who owns a Bot that has gone through the Verification Process when it just came out
+	UserFlagEarlyVerifiedBotDeveloper UserFlag = 1 << 17
+	//UserFlagDiscordCertifiedModerator is a User who has gone through the discord moderator academy and been active in the moderator Guild
+	UserFlagDiscordCertifiedModerator UserFlag = 1 << 18
+	//UserFlagAll is a UserFlag of all flags apart from UserFlagNil ANDed together
+	UserFlagAll = UserFlagDiscordEmployee | UserFlagPartneredServerOwner | UserFlagHypeSquadEvents | UserFlagBugHunterLevel1 | UserFlagHouseBravery | UserFlagHouseBrilliance | UserFlagHouseBalance | UserFlagEarlySupporter | UserFlagTeamUser | UserFlagBugHunterLevel2 | UserFlagVerifiedBot | UserFlagEarlyVerifiedBotDeveloper | UserFlagDiscordCertifiedModerator
+)
+
+//IsValid UserFlag
+func (userFlag UserFlag) IsValid() bool {
+	return UserFlagAll.Contains(userFlag) && userFlag != UserFlagNil
+}
+
+//Contains another UserFlag
+func (userFlag UserFlag) Contains(flags UserFlag) bool {
+	return userFlag&flags == flags
+}
+
+//PremiumType documented at https://discord.com/developers/docs/resources/user#user-object-premium-types
+type PremiumType uint8
+
+const (
+	//PremiumTypeNil is a User without a Nitro subscription
+	PremiumTypeNil PremiumType = iota
+	//PremiumTypeNitroClassic is a User with a Classic Nitro subscription
+	PremiumTypeNitroClassic
+	//PremiumTypeNitro is a User with a Nitro subscription
+	PremiumTypeNitro
+)
+
+//IsValid PremiumType
+func (premiumType PremiumType) IsValid() bool {
+	switch premiumType {
+	case PremiumTypeNitroClassic,
+		PremiumTypeNitro:
+		return true
+	default:
+		return false
+	}
+}
+
 //User struct from https://discord.com/developers/docs/resources/user#user-object
 type User struct {
+	//ID of User
+	ID Snowflake `json:"id"`
+	//Username of User, not unique
+	Username string `json:"username"`
+	//Discriminator of User, 4 suffix digits
+	Discriminator string `json:"discriminator"`
+	//AvatarHash of User
+	AvatarHash ImageHash `json:"avatar"`
+	//IsBot User
+	IsBot bool `json:"bot"`
+	//IsSystemUser maintained by Discord for official communications
+	IsSystemUser bool `json:"system"`
+	//MFAEnabled is if a User has MultiFactorAuthenticated enabled
+	MFAEnabled bool `json:"mfa_enabled"`
+	//BannerHash of User
+	BannerHash ImageHash `json:"banner"`
+	//BannerAccentColor of User as a hexadecimal color code todo: maybe helper function or custom parse for a "color" package color
+	BannerAccentColor int `json:"accent_color"`
+	//Locale of User
+	Locale string `json:"locale"`
+	//IsVerified account (by email)
+	IsVerified bool `json:"is_verified"`
+	//Email of User
+	Email string `json:"email"`
+	//Flags for User (ex: Discord Employee, Early Supporter)
+	Flags UserFlag `json:"flags"`
+	//PremiumType of User (aka nitro type)
+	PremiumType PremiumType `json:"premium_type"`
+	//PublicFlags seen by all User(s)
+	PublicFlags UserFlag `json:"public_flags"`
 }
---
+++ discordprimatives/users_test.go
@@ -0,0 +1,169 @@
+package discordprimatives
+
+import "testing"
+
+func TestUserFlag_IsValid(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name     string
+		userFlag UserFlag
+		want     bool
+	}{
+		{
+			name:     "UserFlagNil",
+			userFlag: UserFlagNil,
+			want:     false,
+		},
+		{
+			name:     "UserFlagDiscordEmployee",
+			userFlag: UserFlagDiscordEmployee,
+			want:     true,
+		},
+		{
+			name:     "UserFlagPartneredServerOwner",
+			userFlag: UserFlagPartneredServerOwner,
+			want:     true,
+		},
+		{
+			name:     "UserFlagHypeSquadEvents",
+			userFlag: UserFlagHypeSquadEvents,
+			want:     true,
+		},
+		{
+			name:     "UserFlagBugHunterLevel1",
+			userFlag: UserFlagBugHunterLevel1,
+			want:     true,
+		},
+		{
+			name:     "UserFlagHouseBravery",
+			userFlag: UserFlagHouseBravery,
+			want:     true,
+		},
+		{
+			name:     "UserFlagHouseBrilliance",
+			userFlag: UserFlagHouseBrilliance,
+			want:     true,
+		},
+		{
+			name:     "UserFlagHouseBalance",
+			userFlag: UserFlagHouseBalance,
+			want:     true,
+		},
+		{
+			name:     "UserFlagEarlySupporter",
+			userFlag: UserFlagEarlySupporter,
+			want:     true,
+		},
+		{
+			name:     "UserFlagTeamUser",
+			userFlag: UserFlagTeamUser,
+			want:     true,
+		},
+		{
+			name:     "UserFlagBugHunterLevel2",
+			userFlag: UserFlagBugHunterLevel2,
+			want:     true,
+		},
+		{
+			name:     "UserFlagVerifiedBot",
+			userFlag: UserFlagVerifiedBot,
+			want:     true,
+		},
+		{
+			name:     "UserFlagEarlyVerifiedBotDeveloper",
+			userFlag: UserFlagEarlyVerifiedBotDeveloper,
+			want:     true,
+		},
+		{
+			name:     "UserFlagDiscordCertifiedModerator",
+			userFlag: UserFlagDiscordCertifiedModerator,
+			want:     true,
+		},
+		{
+			name:     "UserFlagAll",
+			userFlag: UserFlagAll,
+			want:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.userFlag.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUserFlag_Contains(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name     string
+		userFlag UserFlag
+		flags    UserFlag
+		want     bool
+	}{
+		{
+			name:     "UserFlagPartneredServerOwner|UserFlagHypeSquadEvents",
+			userFlag: UserFlagAll,
+			flags:    UserFlagPartneredServerOwner | UserFlagHypeSquadEvents,
+			want:     true,
+		},
+		{
+			name:     "UserFlagAll",
+			userFlag: UserFlagAll,
+			flags:    UserFlagAll,
+			want:     true,
+		},
+		{
+			name:     "UserFlagBugHunterLevel1",
+			userFlag: UserFlagBugHunterLevel1,
+			flags:    UserFlagPartneredServerOwner,
+			want:     false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.userFlag.Contains(tt.flags); got != tt.want {
+				t.Errorf("Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPremiumType_IsValid(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name        string
+		premiumType PremiumType
+		want        bool
+	}{
+		{
+			name:        "PremiumTypeNil",
+			premiumType: PremiumTypeNil,
+			want:        false,
+		},
+		{
+			name:        "PremiumTypeNitroClassic",
+			premiumType: PremiumTypeNitroClassic,
+			want:        true,
+		},
+		{
+			name:        "PremiumTypeNitro",
+			premiumType: PremiumTypeNitro,
+			want:        true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.premiumType.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
