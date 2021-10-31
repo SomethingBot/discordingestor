c7f3cf81ff641f099a092a20ce7a01f9df7333ba
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-31 16:55:48 -0500

    add stages

--- discordprimatives/stages.go
+++ discordprimatives/stages.go
@@ -1,5 +1,37 @@
 package discordprimatives
 
+//PrivacyLevel documented at https://discord.com/developers/docs/resources/stage-instance#stage-instance-object-privacy-level
+type PrivacyLevel int8
+
+const (
+	PrivacyLevelNil PrivacyLevel = iota
+	PrivacyLevelPublic
+	PrivacyLevelGuildOnly
+)
+
+//IsValid PrivacyLevel
+func (privacyLevel PrivacyLevel) IsValid() bool {
+	switch privacyLevel {
+	case PrivacyLevelPublic,
+		PrivacyLevelGuildOnly:
+		return true
+	default:
+		return false
+	}
+}
+
 //StageInstance struct from json, documented at https://discord.com/developers/docs/resources/stage-instance#stage-instance-object
 type StageInstance struct {
+	//ID of StageInstance
+	ID Snowflake `json:"id"`
+	//GuildID of StageInstance
+	GuildID Snowflake `json:"guild_id"`
+	//ChannelID of StageInstance
+	ChannelID Snowflake `json:"channel_id"`
+	//Topic of StageInstance
+	Topic string `json:"topic"`
+	//PrivacyLevel of StageInstance
+	PrivacyLevel PrivacyLevel `json:"privacy_level"`
+	//IsDiscoverableDisabled aka stage discovery
+	IsDiscoverableDisabled bool `json:"discoverable_disabled"`
 }
---
+++ discordprimatives/stages_test.go
@@ -0,0 +1,37 @@
+package discordprimatives
+
+import "testing"
+
+func TestPrivacyLevel_IsValid(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name         string
+		privacyLevel PrivacyLevel
+		want         bool
+	}{
+		{
+			name:         "PrivacyLevelNil",
+			privacyLevel: PrivacyLevelNil,
+			want:         false,
+		},
+		{
+			name:         "PrivacyLevelPublic",
+			privacyLevel: PrivacyLevelPublic,
+			want:         true,
+		},
+		{
+			name:         "PrivacyLevelGuildOnly",
+			privacyLevel: PrivacyLevelGuildOnly,
+			want:         true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.privacyLevel.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
