e71e6a7339aa216c56dc8b3000b1b07c8d270d17
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-31 17:23:43 -0500

    add stickers

--- discordprimatives/stickers.go
+++ discordprimatives/stickers.go
@@ -1,5 +1,77 @@
 package discordprimatives
 
+//StickerType documented at https://discord.com/developers/docs/resources/sticker#sticker-object-sticker-types
+type StickerType uint8
+
+const (
+	//StickerTypeNil is a zero-value StickerType
+	StickerTypeNil StickerType = iota
+	//StickerTypeStandard is a Discord-set Sticker in a pack
+	StickerTypeStandard
+	//StickerTypeGuild is a Sticker uploaded by a User to a Guild
+	StickerTypeGuild
+)
+
+//IsValid StickerType
+func (stickerType StickerType) IsValid() bool {
+	switch stickerType {
+	case StickerTypeStandard,
+		StickerTypeGuild:
+		return true
+	default:
+		return false
+	}
+}
+
+//StickerFormatType documented at https://discord.com/developers/docs/resources/sticker#sticker-object-sticker-format-types
+type StickerFormatType uint8
+
+const (
+	//StickerFormatTypeNil is the default StickerFormatType
+	StickerFormatTypeNil StickerFormatType = iota
+	//StickerFormatTypePNG is a PNG format
+	StickerFormatTypePNG
+	//StickerFormatTypeAPNG is a APNG format
+	StickerFormatTypeAPNG
+	//StickerFormatTypeLOTTIE is a LOTTIE format
+	StickerFormatTypeLOTTIE
+)
+
+func (stickerFormatType StickerFormatType) IsValid() bool {
+	switch stickerFormatType {
+	case StickerFormatTypePNG,
+		StickerFormatTypeAPNG,
+		StickerFormatTypeLOTTIE:
+		return true
+	default:
+		return false
+	}
+}
+
 //Sticker struct from json, documented at https://discord.com/developers/docs/resources/sticker#sticker-object
 type Sticker struct {
+	//ID of Sticker
+	ID Snowflake `json:"id"`
+	//PackID of Sticker, if a standard sticker
+	PackID Snowflake `json:"pack_id"`
+	//Name of Sticker
+	Name string `json:"name"`
+	//Description of Sticker
+	Description string `json:"description"`
+	//Tags for Autocomplete when searching for Sticker (max 200 characters)
+	Tags string `json:"tags"`
+	//Asset Deprecated, previously an Asset hash, now just empty
+	Asset string `json:"asset"`
+	//Type of Sticker
+	Type StickerType `json:"type"`
+	//FormatType of Sticker
+	FormatType StickerFormatType `json:"format_type"`
+	//IsAvailable false if fell below required PremiumTier
+	IsAvailable bool `json:"is_available"`
+	//GuildID of Guild this Sticker was uploaded to
+	GuildID Snowflake `json:"guild_id"`
+	//User that uploaded this Sticker
+	User User `json:"user"`
+	//SortValue this Sticker is in within its pack
+	SortValue int `json:"sort_value"`
 }
---
+++ discordprimatives/stickers_test.go
@@ -0,0 +1,76 @@
+package discordprimatives
+
+import "testing"
+
+func TestStickerType_IsValid(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name        string
+		stickerType StickerType
+		want        bool
+	}{
+		{
+			name:        "StickerTypeNil",
+			stickerType: StickerTypeNil,
+			want:        false,
+		},
+		{
+			name:        "StickerTypeStandard",
+			stickerType: StickerTypeStandard,
+			want:        true,
+		},
+		{
+			name:        "StickerTypeGuild",
+			stickerType: StickerTypeGuild,
+			want:        true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.stickerType.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStickerFormatType_IsValid(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name              string
+		stickerFormatType StickerFormatType
+		want              bool
+	}{
+		{
+			name:              "StickerFormatTypeNil",
+			stickerFormatType: StickerFormatTypeNil,
+			want:              false,
+		},
+		{
+			name:              "StickerFormatTypePNG",
+			stickerFormatType: StickerFormatTypePNG,
+			want:              true,
+		},
+		{
+			name:              "StickerFormatTypeAPNG",
+			stickerFormatType: StickerFormatTypeAPNG,
+			want:              true,
+		},
+		{
+			name:              "StickerFormatTypeLOTTIE",
+			stickerFormatType: StickerFormatTypeLOTTIE,
+			want:              true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.stickerFormatType.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
