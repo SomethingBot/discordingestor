948d52ae11537a3825292541fd1d15d2b529341b
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-10-31 16:29:39 -0500

    add permissions

--- discordprimatives/permissions.go
+++ discordprimatives/permissions.go
@@ -1,4 +1,102 @@
 package discordprimatives
 
 //PermissionFlag documented at https://discord.com/developers/docs/topics/permissions#permissions
-type PermissionFlag uint64 //todo: fill up permissions constants
+//Check PermissionFlag.IsValid() then PermissionFlag.HasPermission()
+type PermissionFlag uint64
+
+const (
+	//PermissionFlagNil is the default PermissionFlag value
+	PermissionFlagNil PermissionFlag = 0
+	//PermissionFlagCreateInstantInvite allows instant invite creation in ChannelTypeGuildText, ChannelTypeGuildVoice, & ChannelTypeGuildStageVoice
+	PermissionFlagCreateInstantInvite PermissionFlag = 1 << (iota - 1)
+	//PermissionFlagKickMembers allows kicking GuildMember(s)
+	PermissionFlagKickMembers
+	//PermissionFlagBanMembers allows banning GuildMember(s)
+	PermissionFlagBanMembers
+	//PermissionFlagAdministrator allows all PermissionFlag(s) and bypasses all Overwrite(s)
+	PermissionFlagAdministrator
+	//PermissionFlagManageChannels allows management and editing of Channel types ChannelTypeGuildText, ChannelTypeGuildVoice, & ChannelTypeGuildStageVoice
+	PermissionFlagManageChannels
+	//PermissionFlagManageGuild allows management and editing of a Guild
+	PermissionFlagManageGuild
+	//PermissionFlagAddReactions allows adding Reaction(s) (an emoji) to a message in a ChannelTypeGuildText
+	PermissionFlagAddReactions
+	//PermissionFlagViewAuditLog allows viewing AuditLog
+	PermissionFlagViewAuditLog
+	//PermissionFlagPrioritySpeaker allows being a PrioritySpeaker in a ChannelTypeGuildVoice
+	PermissionFlagPrioritySpeaker
+	//PermissionFlagStream allows User to start a Stream in a ChannelTypeGuildVoice
+	PermissionFlagStream
+	//PermissionFlagViewChannel allows a User to view a Channel
+	PermissionFlagViewChannel
+	//PermissionFlagSendMessages allows a User to send messages in a ChannelTypeGuildText
+	PermissionFlagSendMessages
+	//PermissionFlagSendTTSMessage allows a User to send Text-To-Speech messages in a ChannelTypeGuildText
+	PermissionFlagSendTTSMessage
+	//PermissionFlagManageMessages allows a User to manage a ChannelTypeGuildText messages
+	PermissionFlagManageMessages
+	//PermissionFlagEmbedLinks makes links sent by User with this PermissionFlag to be embedded automatically
+	PermissionFlagEmbedLinks
+	//PermissionFlagAttachFiles allows a User to upload images and files
+	PermissionFlagAttachFiles
+	//PermissionFlagReadMessageHistory allows a User to read message history in a ChannelTypeGuildText
+	PermissionFlagReadMessageHistory
+	//PermissionFlagMentionEveryone allows a User use the @everyone, @here, and all @roles
+	PermissionFlagMentionEveryone
+	//PermissionFlagUseExternalEmojis allows a User to use Emoji(s) from another Guild
+	PermissionFlagUseExternalEmojis
+	//PermissionFlagViewGuildInsights allows a User to view Guild Insights
+	PermissionFlagViewGuildInsights
+	//PermissionFlagConnect allows a User to connect to a ChannelTypeGuildVoice and ChannelTypeGuildStageVoice
+	PermissionFlagConnect
+	//PermissionFlagSpeak allows a User to speak in a ChannelTypeGuildVoice
+	PermissionFlagSpeak
+	//PermissionFlagMuteMembers allows a User to mute another User in a ChannelTypeGuildVoice and ChannelTypeGuildStageVoice
+	PermissionFlagMuteMembers
+	//PermissionFlagDeafenMembers allows a User to deafen another User in a ChannelTypeGuildVoice and ChannelTypeGuildStageVoice
+	PermissionFlagDeafenMembers
+	//PermissionFlagMoveMembers allows a User to move another User to another ChannelTypeGuildVoice or ChannelTypeGuildStageVoice
+	PermissionFlagMoveMembers
+	//PermissionFlagUseVoiceActivityDetection allows a User to use VoiceActivity to show when they are speaking
+	PermissionFlagUseVoiceActivityDetection
+	//PermissionFlagChangeNickname allows a User to change their User.Nickname
+	PermissionFlagChangeNickname
+	//PermissionFlagManageNicknames allows a User to change others User.Nickname
+	PermissionFlagManageNicknames
+	//PermissionFlagManageRoles allows a User to manage Role(s)
+	PermissionFlagManageRoles
+	//PermissionFlagManageWebhooks allows a User to manage Webhooks in a ChannelTypeGuildText
+	PermissionFlagManageWebhooks
+	//PermissionFlagManageEmojisAndStickers allows a User to manage Emoji(s) and Sticker(s)
+	PermissionFlagManageEmojisAndStickers
+	//PermissionFlagUseApplicationCommands allows a User to use application commands (slash commands and context menus)
+	PermissionFlagUseApplicationCommands
+	//PermissionFlagRequestToSpeak allows a User to request to speak in a ChannelTypeGuildStageVoice
+	PermissionFlagRequestToSpeak
+	//PermissionFlagSkippedNotReal is a fake flag because discord skips 1<<33
+	PermissionFlagSkippedNotReal
+	//PermissionFlagManageThreads allows a User to manage Thread(s)
+	PermissionFlagManageThreads
+	//PermissionFlagCreatePublicThreads allows a User create a Thread
+	PermissionFlagCreatePublicThreads
+	//PermissionFlagCreatePrivateThreads allows a User to create a private Thread
+	PermissionFlagCreatePrivateThreads
+	//PermissionFlagUseExternalStickers allows a User to use another Guild's Sticker(s)
+	PermissionFlagUseExternalStickers
+	//PermissionFlagSendMessageInThreads allows a User to send messages in a Thread
+	PermissionFlagSendMessageInThreads
+	//PermissionFlagStartEmbeddedActivities allows a User to launch Activities (applications with the Embedded flag) in a ChannelTypeGuildVoice
+	PermissionFlagStartEmbeddedActivities
+	//PermissionFlagALL is a ANDed of all valid PermissionFlag
+	PermissionFlagALL PermissionFlag = (1 << (iota - 1)) - 1
+)
+
+//IsValid PermissionFlag
+func (permissionFlag PermissionFlag) IsValid() bool {
+	return PermissionFlagALL.Contains(permissionFlag) && permissionFlag != PermissionFlagNil && !permissionFlag.Contains(PermissionFlagSkippedNotReal) || permissionFlag == PermissionFlagALL
+}
+
+//Contains another PermissionFlag
+func (permissionFlag PermissionFlag) Contains(flags PermissionFlag) bool {
+	return permissionFlag&flags == flags
+}
---
+++ discordprimatives/permissions_test.go
@@ -0,0 +1,504 @@
+package discordprimatives
+
+import "testing"
+
+func TestPermissionFlag_IsValid(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name           string
+		permissionFlag PermissionFlag
+		want           bool
+	}{
+		{
+			name:           "PermissionFlagNil",
+			permissionFlag: PermissionFlagNil,
+			want:           false,
+		},
+		{
+			name:           "PermissionFlagCreateInstantInvite",
+			permissionFlag: PermissionFlagCreateInstantInvite,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagKickMembers",
+			permissionFlag: PermissionFlagKickMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagBanMembers",
+			permissionFlag: PermissionFlagBanMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagAdministrator",
+			permissionFlag: PermissionFlagAdministrator,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageChannels",
+			permissionFlag: PermissionFlagManageChannels,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageGuild",
+			permissionFlag: PermissionFlagManageGuild,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagAddReactions",
+			permissionFlag: PermissionFlagAddReactions,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagViewAuditLog",
+			permissionFlag: PermissionFlagViewAuditLog,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagPrioritySpeaker",
+			permissionFlag: PermissionFlagPrioritySpeaker,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagStream",
+			permissionFlag: PermissionFlagStream,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagViewChannel",
+			permissionFlag: PermissionFlagViewChannel,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSendMessages",
+			permissionFlag: PermissionFlagSendMessages,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSendTTSMessage",
+			permissionFlag: PermissionFlagSendTTSMessage,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageMessages",
+			permissionFlag: PermissionFlagManageMessages,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagEmbedLinks",
+			permissionFlag: PermissionFlagEmbedLinks,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagAttachFiles",
+			permissionFlag: PermissionFlagAttachFiles,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagReadMessageHistory",
+			permissionFlag: PermissionFlagReadMessageHistory,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagMentionEveryone",
+			permissionFlag: PermissionFlagMentionEveryone,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseExternalEmojis",
+			permissionFlag: PermissionFlagUseExternalEmojis,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagViewGuildInsights",
+			permissionFlag: PermissionFlagViewGuildInsights,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagConnect",
+			permissionFlag: PermissionFlagConnect,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSpeak",
+			permissionFlag: PermissionFlagSpeak,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagMuteMembers",
+			permissionFlag: PermissionFlagMuteMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagDeafenMembers",
+			permissionFlag: PermissionFlagDeafenMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagMoveMembers",
+			permissionFlag: PermissionFlagMoveMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseVoiceActivityDetection",
+			permissionFlag: PermissionFlagUseVoiceActivityDetection,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagChangeNickname",
+			permissionFlag: PermissionFlagChangeNickname,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageNicknames",
+			permissionFlag: PermissionFlagManageNicknames,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageRoles",
+			permissionFlag: PermissionFlagManageRoles,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageWebhooks",
+			permissionFlag: PermissionFlagManageWebhooks,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageEmojisAndStickers",
+			permissionFlag: PermissionFlagManageEmojisAndStickers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseApplicationCommands",
+			permissionFlag: PermissionFlagUseApplicationCommands,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagRequestToSpeak",
+			permissionFlag: PermissionFlagRequestToSpeak,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSkippedNotReal",
+			permissionFlag: PermissionFlagSkippedNotReal,
+			want:           false,
+		},
+		{
+			name:           "PermissionFlagManageThreads",
+			permissionFlag: PermissionFlagManageThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagCreatePublicThreads",
+			permissionFlag: PermissionFlagCreatePublicThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagCreatePrivateThreads",
+			permissionFlag: PermissionFlagCreatePrivateThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseExternalStickers",
+			permissionFlag: PermissionFlagUseExternalStickers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSendMessageInThreads",
+			permissionFlag: PermissionFlagSendMessageInThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagStartEmbeddedActivities",
+			permissionFlag: PermissionFlagStartEmbeddedActivities,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagALL",
+			permissionFlag: PermissionFlagALL,
+			want:           true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.permissionFlag.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPermissionFlag_Contains(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name           string
+		permissionFlag PermissionFlag
+		flags          PermissionFlag
+		want           bool
+	}{
+		{
+			name:           "PermissionFlagNil",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagNil,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagCreateInstantInvite",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagCreateInstantInvite,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagKickMembers",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagKickMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagBanMembers",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagBanMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagAdministrator",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagAdministrator,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageChannels",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageChannels,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageGuild",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageGuild,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagAddReactions",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagAddReactions,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagViewAuditLog",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagViewAuditLog,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagPrioritySpeaker",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagPrioritySpeaker,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagStream",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagStream,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagViewChannel",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagViewChannel,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSendMessages",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagSendMessages,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSendTTSMessage",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagSendTTSMessage,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageMessages",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageMessages,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagEmbedLinks",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagEmbedLinks,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagAttachFiles",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagAttachFiles,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagReadMessageHistory",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagReadMessageHistory,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagMentionEveryone",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagMentionEveryone,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseExternalEmojis",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagUseExternalEmojis,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagViewGuildInsights",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagViewGuildInsights,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagConnect",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagConnect,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSpeak",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagSpeak,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagMuteMembers",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagMuteMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagDeafenMembers",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagDeafenMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagMoveMembers",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagMoveMembers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseVoiceActivityDetection",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagUseVoiceActivityDetection,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagChangeNickname",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagChangeNickname,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageNicknames",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageNicknames,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageRoles",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageRoles,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageWebhooks",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageWebhooks,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageEmojisAndStickers",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageEmojisAndStickers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseApplicationCommands",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagUseApplicationCommands,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagRequestToSpeak",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagRequestToSpeak,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSkippedNotReal",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagSkippedNotReal,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagManageThreads",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagManageThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagCreatePublicThreads",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagCreatePublicThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagCreatePrivateThreads",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagCreatePrivateThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagUseExternalStickers",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagUseExternalStickers,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagSendMessageInThreads",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagSendMessageInThreads,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagStartEmbeddedActivities",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagStartEmbeddedActivities,
+			want:           true,
+		},
+		{
+			name:           "PermissionFlagALL",
+			permissionFlag: PermissionFlagALL,
+			flags:          PermissionFlagALL,
+			want:           true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+			if got := tt.permissionFlag.Contains(tt.flags); got != tt.want {
+				t.Errorf("Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
