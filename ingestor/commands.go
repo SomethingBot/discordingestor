8694c4330e2f05f5eacf6fe4d7522e7ef96f993a
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-11-22 15:59:14 -0600

    use guild create for sample

--- ingestor/commands.go
+++ ingestor/commands.go
@@ -2,6 +2,6 @@ package ingestor
 
 import "github.com/SomethingBot/discordingestor/discord/primitives"
 
-func (ingestor *Ingestor) handleMessages(message primitives.GatewayEventMessageCreate) {
-	ingestor.logger.Printf("Saw message (%v)", message.Content)
+func (ingestor *Ingestor) handleMessages(gc primitives.GatewayEventGuildCreate) {
+	ingestor.logger.Printf("Saw guild create (%v)", gc.Name)
 }
