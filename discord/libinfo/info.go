8feb7a073f73cda48e740e3c276bb9ec779c4972
SlightlySideways <82488311+slightlysideways@users.noreply.github.com>
2021-11-24 14:42:05 -0600

    bump version

--- discord/libinfo/info.go
+++ discord/libinfo/info.go
@@ -4,7 +4,7 @@ const (
 	//BotLibraryUrl is the URL of the library
 	BotLibraryUrl = "github.com/SomethingBot/discordingestor/discord"
 	//BotLibraryVersion is the version of the library; this is the git tag
-	BotLibraryVersion = "0.0.0"
+	BotLibraryVersion = "0.0.2"
 	//BotUserAgent is the UserAgent of all library http requests
 	BotUserAgent = "DiscordBot (" + BotLibraryUrl + ", " + BotLibraryVersion + ")"
 )
