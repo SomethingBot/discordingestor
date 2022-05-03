404d04dc7541187ce75e781815effae1512b9c03
SlightlySideways <82488311+SlightlySideways@users.noreply.github.com>
2022-05-02 20:26:59 -0500

    add GetGatewayWebsocketInformation (#2)
    * add discordwebapi.GetGatewayWebsocketInformation

--- discord/discordwebapi/gateway.go
+++ discord/discordwebapi/gateway.go
@@ -4,9 +4,76 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/SomethingBot/discordingestor/discord/libinfo"
+	"io"
 	"net/http"
 	"net/url"
 )
+
+type GatewayWebsocketInformationSessionStartLimit struct {
+	Total          int `json:"total"`
+	Remaining      int `json:"remaining"`
+	ResetAfter     int `json:"reset_after"`
+	MaxConcurrency int `json:"max_concurrency"`
+}
+
+type GatewayWebsocketInformation struct {
+	Url               string                                       `json:"url"`
+	Shards            int                                          `json:"shards"`
+	SessionStartLimit GatewayWebsocketInformationSessionStartLimit `json:"session_start_limit"`
+}
+
+//GetGatewayWebsocketInformation for a bot to connect to the Discord websocket, pass discordApiGatewayURL as "" to use default
+func GetGatewayWebsocketInformation(discordApiGatewayURL string, apiKey string) (GatewayWebsocketInformation, error) {
+	if discordApiGatewayURL == "" {
+		discordApiGatewayURL = "https://discord.com/api/gateway/bot"
+	}
+
+	req, err := http.NewRequest("GET", discordApiGatewayURL, nil)
+	if err != nil {
+		return GatewayWebsocketInformation{}, err
+	}
+	req.Header.Set("User-Agent", libinfo.BotUserAgent)
+	req.Header.Add("Authorization", "Bot "+apiKey)
+
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		return GatewayWebsocketInformation{}, err
+	}
+
+	defer func() {
+		if err != nil {
+			err2 := resp.Body.Close()
+			if err2 != nil {
+				err = fmt.Errorf("could not close body (%v) after error (%w)", err2, err)
+			}
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		var errorData []byte
+		errorData, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return GatewayWebsocketInformation{}, fmt.Errorf("could not readall data from resp.Body (%w)", err)
+		}
+		return GatewayWebsocketInformation{}, WebAPIError{JsonData: errorData} //todo: convert to real error type
+	}
+
+	var gwi GatewayWebsocketInformation
+
+	decoder := json.NewDecoder(resp.Body)
+	decoder.DisallowUnknownFields()
+	err = decoder.Decode(&gwi)
+	if err != nil {
+		return GatewayWebsocketInformation{}, fmt.Errorf("could not decode json (%w)", err)
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return GatewayWebsocketInformation{}, fmt.Errorf("could not close body (%w)", err)
+	}
+
+	return gwi, nil
+}
 
 //GetGatewayWebsocketURI returns the current Discord Gateway WSS URL, pass discordApiGatewayURL as "" to use default  //todo: make it so test doesn't have to hit server
 func GetGatewayWebsocketURI(discordApiGatewayURL string) (url.URL, error) {
